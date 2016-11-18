3bc89650793eebec3d03c2e10fd53c4d0dc34577
nameqiaohe <nameqiaohe@126.com>
2016-11-18 16:43:21 +0800

    环境搭建及ant的简单了解

---
+++ webserver/first_webserver/print_hello.java
@@ -0,0 +1,12 @@
+/*####################################################
+# File Name: print_hello.java
+# Author: xxx
+# Email: xxx@126.com
+# Create Time: 2016-11-18 15:32:36
+# Last Modified: 2016-11-18 15:32:36
+####################################################*/
+class print_hello{
+	public static void main(String args[]){
+		System.out.println("Hello World(Java)!!!");
+	}
+}
