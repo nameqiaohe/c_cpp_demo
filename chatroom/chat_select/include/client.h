179d76261d297103dcad0917e5c1db38d5114090
nameqiaohe <nameqiaohe@126.com>
2016-12-10 21:01:04 +0800

    简单的聊天室

---
+++ chatroom/chat_select/include/client.h
@@ -0,0 +1,15 @@
+/*####################################################
+# File Name: ../include/client.h
+# Author: xxx
+# Email: xxx@126.com
+# Create Time: 2016-12-10 16:49:46
+# Last Modified: 2016-12-10 18:18:03
+####################################################*/
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+void handle(int connfd);
+
+#endif//CLIENT_H
